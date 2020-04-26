de500cd2cfdd60b77870da5e7b8fee9ad72d0762
C. Greyston Brady <c.greyston.brady@gmail.com>
2020-04-26 13:46:28 -0700

    Fixed main and added Registry.hpp to build

---
+++ Loom-Deconstructed/Loom-Deconstructed/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  Loom-Deconstructed
+//
+//  Created by Greyston Brady on 4/26/20.
+//  Copyright © 2020 OPEnS. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+}
